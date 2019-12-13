ddef34a7a791e82f6980c72fdbaa041b8c01c02c
clouu27 <jasperaquino1412@gmail.com>
2019-12-13 18:12:43 +0800

    Modified InputStream read to dynamically load rom; added new instructions

--- CpuEmulation.java
+++ CpuEmulation.java
@@ -22,12 +22,12 @@ public class CpuEmulation
 	
 	/////   MAIN EMULATION   /////
 	public void emulate8080() {
-		while(PC.value < 0x4000) {
+		while(PC.value < Main.PROGRAM_LENGTH) {
 			// store opcode
 			int opcode = PC.value;
 			
 			// HL (M)
-			int addr = (H.value << 8) | L.value;
+			int addr = ((H.value << 8) | L.value);
 			
 			// increment PC
 			PC.value++;
@@ -151,9 +151,198 @@ public class CpuEmulation
 				case 0x3e:
 					MVI(opcode, A);
 					break; // MVI A, D8
+					
+				case 0x40:
+					B.value = B.value;
+					break; // MOV B, B
+				case 0x41:
+					B.value = C.value;
+					break; // MOV B, C
+				case 0x42:
+					B.value = D.value;
+					break; // MOV B, D
+				case 0x43:
+					B.value = E.value;
+					break; // MOV B, E
+				case 0x44:
+					B.value = H.value;
+					break; // MOV B, H
+				case 0x45:
+					B.value = L.value;
+					break; // MOV B, L
+				case 0x46:
+					B.value = memory[addr];
+					break; // MOV B, M
+				case 0x47:
+					B.value = A.value;
+					break; // MOV B, A
+				case 0x48:
+					C.value = B.value;
+					break; // MOV C, B
+				case 0x49:
+					C.value = C.value;
+					break; // MOV C, C
+				case 0x4a:
+					C.value = D.value;
+					break; // MOV C, D
+				case 0x4b:
+					C.value = E.value;
+					break; // MOV C, E
+				case 0x4c:
+					C.value = H.value;
+					break; // MOV C, H
+				case 0x4d:
+					C.value = L.value;
+					break; // MOV C, L
+				case 0x4e:
+					C.value = memory[addr];
+					break; // MOV C, M
+				case 0x4f:
+					C.value = A.value;
+					break; // MOV C, A
+				case 0x50:
+					D.value = B.value;
+					break; // MOV D, B
+				case 0x51:
+					D.value = C.value;
+					break; // MOV D, C
+				case 0x52:
+					D.value = D.value;
+					break; // MOV D, D
+				case 0x53:
+					D.value = E.value;
+					break; // MOV D, E
+				case 0x54:
+					D.value = H.value;
+					break; // MOV D, H
+				case 0x55:
+					D.value = L.value;
+					break; // MOV D, L
+				case 0x56:
+					D.value = memory[addr];
+					break; // MOV D, M
+				case 0x57:
+					D.value = A.value;
+					break; // MOV D, A
+				case 0x58:
+					E.value = B.value;
+					break; // MOV E, B
+				case 0x59:
+					E.value = C.value;
+					break; // MOV E, C
+				case 0x5a:
+					E.value = D.value;
+					break; // MOV E, D
+				case 0x5b:
+					E.value = E.value;
+					break; // MOV E, E
+				case 0x5c:
+					E.value = H.value;
+					break; // MOV E, H
+				case 0x5d:
+					E.value = L.value;
+					break; // MOV E, L
+				case 0x5e:
+					E.value = memory[addr];
+					break; // MOV E, M
+				case 0x5f:
+					E.value = A.value;
+					break; // MOV E, A
+				case 0x60:
+					H.value = B.value;
+					break; // MOV H, B
+				case 0x61:
+					H.value = C.value;
+					break; // MOV H, C
+				case 0x62:
+					H.value = D.value;
+					break; // MOV H, D
+				case 0x63:
+					H.value = E.value;
+					break; // MOV H, E
+				case 0x64:
+					H.value = H.value;
+					break; // MOV H, H
+				case 0x65:
+					H.value = L.value;
+					break; // MOV H, L
+				case 0x66:
+					H.value = memory[addr];
+					break; // MOV H, M
+				case 0x67:
+					H.value = A.value;
+					break; // MOV H, A
+				case 0x68:
+					L.value = B.value;
+					break; // MOV L, B
+				case 0x69:
+					L.value = C.value;
+					break; // MOV L, C
+				case 0x6a:
+					L.value = D.value;
+					break; // MOV L, D
+				case 0x6b:
+					L.value = E.value;
+					break; // MOV L, E
+				case 0x6c:
+					L.value = H.value;
+					break; // MOV L, H
+				case 0x6d:
+					L.value = L.value;
+					break; // MOV L, L
+				case 0x6e:
+					L.value = memory[addr];
+					break; // MOV L, M
+				case 0x6f:
+					L.value = A.value;
+					break; // MOV L, A
+				case 0x70:
+					memory[addr] = B.value;
+					break; // MOV M, B
+				case 0x71:
+					memory[addr] = C.value;
+					break; // MOV M, C
+				case 0x72:
+					memory[addr] = D.value;
+					break; // MOV M, D
+				case 0x73:
+					memory[addr] = E.value;
+					break; // MOV M, E
+				case 0x74:
+					memory[addr] = H.value;
+					break; // MOV M, H
+				case 0x75:
+					memory[addr] = L.value;
+					break; // MOV M, L
+				case 0x76:
+					return; // HLT
 				case 0x77:
 					memory[addr] = A.value;
 					break; // MOV M, A
+				case 0x78:
+					A.value = B.value;
+					break; // MOV A, B
+				case 0x79:
+					A.value = C.value;
+					break; // MOV A, C
+				case 0x7a:
+					A.value = D.value;
+					break; // MOV A, D
+				case 0x7b:
+					A.value = E.value;
+					break; // MOV A, E
+				case 0x7c:
+					A.value = H.value;
+					break; // MOV A, H
+				case 0x7d:
+					A.value = L.value;
+					break; // MOV A, L
+				case 0x7e:
+					A.value = memory[addr];
+					break; // MOV A, M
+				case 0x7f:
+					A.value = A.value;
+					break; // MOV A, A
 				case 0xc2:
 					if(Z.flag == 0) {
 						PC.value = (getInstruction(opcode + 2) << 8) | getInstruction(opcode + 1);
@@ -164,9 +353,23 @@ public class CpuEmulation
 				case 0xc3:
 					PC.value = (getInstruction(opcode + 2) << 8) | getInstruction(opcode + 1);
 					break; // JMP adr
+				case 0xc9:
+					RET();
+					break; // RET
 				case 0xcd:
 					CALL(opcode);
 					break; // CALL adr
+				case 0xe4:
+					if (P.flag == 0) {
+						CALL(opcode);
+					} else {
+						PC.value += 2;
+					}
+					break; // CPO adr
+				case 0xfe:
+					CMP(memory[opcode + 1]);
+					break; // CPI D8
+
 				default:
 					return;
 			}
@@ -283,14 +486,209 @@ public class CpuEmulation
 			case 0x35:
 				inst = "DCR M";
 				break;
+			case 0x36:
+				inst = "MVI M, #" + toHex02(getInstruction(opcode + 1));
+				break;
 			case 0x3b:
 				inst = "DCX SP";
 				break;
 			case 0x3d:
 				inst = "DCR A";
 				break;
+			case 0x3e:
+				inst = "MVI A, #" + toHex02(getInstruction(opcode + 1));
+				break;
+			case 0x40:
+				inst = "MOV B, B";
+				break;
+			case 0x41:
+				inst = "MOV B, C";
+				break;
+			case 0x42:
+				inst = "MOV B, D";
+				break;
+			case 0x43:
+				inst = "MOV B, E";
+				break;
+			case 0x44:
+				inst = "MOV B, H";
+				break;
+			case 0x45:
+				inst = "MOV B, L";
+				break;
+			case 0x46:
+				inst = "MOV B, M";
+				break;
+			case 0x47:
+				inst = "MOV B, A";
+				break;
+			case 0x48:
+				inst = "MOV C, B";
+				break;
+			case 0x49:
+				inst = "MOV C, C";
+				break;
+			case 0x4a:
+				inst = "MOV C, D";
+				break;
+			case 0x4b:
+				inst = "MOV C, E";
+				break;
+			case 0x4c:
+				inst = "MOV C, H";
+				break;
+			case 0x4d:
+				inst = "MOV C, L";
+				break;
+			case 0x4e:
+				inst = "MOV C, M";
+				break;
+			case 0x4f:
+				inst = "MOV C, A";
+				break;
+			case 0x50:
+				inst = "MOV D, B";
+				break;
+			case 0x51:
+				inst = "MOV D, C";
+				break;
+			case 0x52:
+				inst = "MOV D, D";
+				break;
+			case 0x53:
+				inst = "MOV D, E";
+				break;
+			case 0x54:
+				inst = "MOV D, H";
+				break;
+			case 0x55:
+				inst = "MOV D, L";
+				break;
+			case 0x56:
+				inst = "MOV D, M";
+				break;
+			case 0x57:
+				inst = "MOV D, A";
+				break;
+			case 0x58:
+				inst = "MOV E, B";
+				break;
+			case 0x59:
+				inst = "MOV E, C";
+				break;
+			case 0x5a:
+				inst = "MOV E, D";
+				break;
+			case 0x5b:
+				inst = "MOV E, E";
+				break;
+			case 0x5c:
+				inst = "MOV E, H";
+				break;
+			case 0x5d:
+				inst = "MOV E, L";
+				break;
+			case 0x5e:
+				inst = "MOV E, M";
+				break;
+			case 0x5f:
+				inst = "MOV E, A";
+				break;
+			case 0x60:
+				inst = "MOV H, B";
+				break;
+			case 0x61:
+				inst = "MOV H, C";
+				break;
+			case 0x62:
+				inst = "MOV H, D";
+				break;
+			case 0x63:
+				inst = "MOV H, E";
+				break;
+			case 0x64:
+				inst = "MOV H, H";
+				break;
+			case 0x65:
+				inst = "MOV H, L";
+				break;
+			case 0x66:
+				inst = "MOV H, M";
+				break;
+			case 0x67:
+				inst = "MOV H, A";
+				break;
+			case 0x68:
+				inst = "MOV L, B";
+				break;
+			case 0x69:
+				inst = "MOV L, C";
+				break;
+			case 0x6a:
+				inst = "MOV L, D";
+				break;
+			case 0x6b:
+				inst = "MOV L, E";
+				break;
+			case 0x6c:
+				inst = "MOV L, H";
+				break;
+			case 0x6d:
+				inst = "MOV L, L";
+				break;
+			case 0x6e:
+				inst = "MOV L, M";
+				break;
+			case 0x6f:
+				inst = "MOV L, A";
+				break;
+			case 0x70:
+				inst = "MOV M, B";
+				break;
+			case 0x71:
+				inst = "MOV M, C";
+				break;
+			case 0x72:
+				inst = "MOV M, D";
+				break;
+			case 0x73:
+				inst = "MOV M, E";
+				break;
+			case 0x74:
+				inst = "MOV M, H";
+				break;
+			case 0x75:
+				inst = "MOV M, L";
+				break;
+			case 0x76:
+				inst = "MOV M, M";
+				break;
 			case 0x77:
 				inst = "MOV M, A";
+				break;
+			case 0x78:
+				inst = "MOV A, B";
+				break;
+			case 0x79:
+				inst = "MOV A, C";
+				break;
+			case 0x7a:
+				inst = "MOV A, D";
+				break;
+			case 0x7b:
+				inst = "MOV A, E";
+				break;
+			case 0x7c:
+				inst = "MOV A, H";
+				break;
+			case 0x7d:
+				inst = "MOV A, L";
+				break;
+			case 0x7e:
+				inst = "MOV A, M";
+				break;
+			case 0x7f:
+				inst = "MOV A, A";
 				break;
 			case 0xc2:
 				inst = "JNZ $" + toHex04((getInstruction(opcode+2) << 8) + getInstruction(opcode+1)) + " | ZF: " + Z.flag;
@@ -298,8 +696,17 @@ public class CpuEmulation
 			case 0xc3:
 				inst = "JMP $" + toHex04((getInstruction(opcode+2) << 8) + getInstruction(opcode+1));
 				break;
+			case 0xc9:
+				inst = "RET";
+				break;
 			case 0xcd:
 				inst = "CALL $" + toHex04((getInstruction(opcode+2) << 8) + getInstruction(opcode+1));
+				break;
+			case 0xe4:
+				inst = "CPO $" + toHex04((getInstruction(opcode + 2) << 8) + getInstruction(opcode + 1));
+				break;
+			case 0xfe:
+				inst = "CPI #" + toHex02(getInstruction(opcode + 1));
 				break;
 			default:
 				inst = "" + toHex02(getInstruction(opcode)) + " is not implemented!";
@@ -319,9 +726,26 @@ public class CpuEmulation
 		PC.value = (getInstruction(opcode + 2) << 8) | getInstruction(opcode + 1);
 	}
 	
+	// Dedicate individual flag checks
+	private void CMP(int opbyte) {
+		// a + (two comp. immediate)
+		// complement — defined also as "another set" e.g. another set of binary 1 is binary 0!
+		// similar to a - immediate
+		// int twoComp = ((~opbyte & 0xff) + 1);
+		int res = A.value - opbyte;
+		
+		Z.flag = (res == 0) ? (byte) 1 : 0;
+		S.flag = ((res & 0x80) == 0x80) ? (byte) 1 : 0;
+		P.flag = parityFlag(res & 0xff);  // ensuring only checks for 8-bit variable
+		CY.flag = (res > 0xff) ? 0 : (byte) 1; // complement carry (inverse)
+		AC.flag = (res > 0x9) ? (byte) 1 : 0;
+		
+		PC.value++;
+	}
+	
 	private void DCR(Component reg) {
 		reg.value--;
-		flags_zsp_ac(reg.value);
+		flags_simple(reg.value);
 		reg.value &= 0xff;
 	}
 	
@@ -346,7 +770,7 @@ public class CpuEmulation
 	
 	private void INR(Component reg) {
 		reg.value++;
-		flags_zsp_ac(reg.value);
+		flags_simple(reg.value);
 		reg.value &= 0xff; // ensure it only takes 8-bit
 	}
 	
@@ -381,10 +805,20 @@ public class CpuEmulation
 			rp[0].value = ((getInstruction(opcode + 2) << 8) | getInstruction(opcode + 1)) & 0xffff;
 		}
 		
+		PC.value += 2;
+		
 	}
 	
 	private void MVI(int opcode, Component reg) {
 		reg.value = getInstruction(opcode + 1);
+		
+		PC.value++;
+	}
+	
+	private void RET() {
+		int addr = (memory[SP.value + 1] << 8) | memory[SP.value];
+		SP.value += 2;
+		PC.value = addr;
 	}
 	
 	
@@ -392,25 +826,27 @@ public class CpuEmulation
 	
 	private void DCR(int address) {
 		memory[address]--;
-		flags_zsp_ac(memory[address]);
+		flags_simple(memory[address]);
 		memory[address] &= 0xff;
 	}
 	
 	private void INR(int address) {
 		memory[address]++;
-		flags_zsp_ac(memory[address]);
+		flags_simple(memory[address]);
 		memory[address] &= 0xff; // ensure it only takes 8-bit
 	}
 	
 	private void MVI(int opcode, int address) {
 		memory[address] = getInstruction(opcode + 1);
+		
+		PC.value++;
 	}
 	
 	///  FLAGS  ///
-	private void flags_zsp_ac(int result) {
+	private void flags_simple(int result) {
 		Z.flag = (result == 0) ? (byte) 1 : 0;
 		S.flag = ((result & 0x80) == 0x80) ? (byte) 1 : 0;
-		P.flag = parityFlag(result);
+		P.flag = parityFlag(result & 0xff);  // ensuring only checks for 8-bit variable
 		AC.flag = (result > 0x9) ? (byte) 1 : 0;
 	}
 	
--- Main.java
+++ Main.java
@@ -4,9 +4,11 @@ import Cpu.*;
 
 public class Main
 {
+	public static final int PROGRAM_LENGTH = 0x4000;
+	
 	static CpuEmulation cpu;
 	
-	final static String STORAGE_INTERNAL = "~/src/"; // Change path
+	final static String STORAGE_INTERNAL = "/sdcard/AppProjects/raw8080v2/src/";
 	final static String FILE_NAME = "invaders";
 	
 	public static void main(String[] args) {
@@ -16,27 +18,19 @@ public class Main
 	// MAIN
 	public static void startEmulator(String romName) {
 		initRom(romName);
-		cpu = new CpuEmulation(loadRom(romName));
+		cpu = new CpuEmulation(loadSplitRom());
 	}
 	
 	// ROM
 	private static void initRom(String romName) {
-		try
-		{
 			RomInfo.title = romName;
-			RomInfo.length = getFileLength(romName);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-			System.exit(0);
-		}
+			RomInfo.length = PROGRAM_LENGTH;
 	}
 	
 	// Load ROM
-	private static int[] loadRom(String romName) {
+	private static int[] loadRoms(String romName) {
 		InputStream file = null;
-		int tmp[] = new int[RomInfo.length + 0x2000];
+		int tmp[] = new int[PROGRAM_LENGTH];
 		
 		try
 		{
@@ -59,6 +53,59 @@ public class Main
 		}
 
 		return tmp;
+	}
+	
+	// LOAD SPLITROM
+	private static int[] loadSplitRom() {
+		String[] romName = {
+			"invaders.h", "invaders.g", "invaders.f", "invaders.e"
+		};
+		
+		int[] romAddr = {
+			0x0, 0x800, 0x1000, 0x1800
+		};
+		
+		// prepare empty container
+		int[] holder = new int[PROGRAM_LENGTH];
+		
+		for(int i = 0; i < romName.length; i++) {
+			InputStream is = openFile(romName[i]);
+			int readFile = 0;
+			
+			int currentAddr = romAddr[i];
+			
+			try
+			{
+				int counter = 0;
+				
+				while ((readFile = is.read()) != -1)
+				{
+					holder[currentAddr + counter] = readFile;
+					counter++;
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		return holder;
+	}
+	
+	// Inputstream
+	private static InputStream openFile(String romName) {
+		try
+		{
+			return new FileInputStream(STORAGE_INTERNAL + romName);
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		return null;
 	}
 	
 	// Return file length
