721b7e6bbfc45136d154a42ff2d06a8698207dc6
clouu27 <jasperaquino1412@gmail.com>
2019-12-22 23:08:26 +0800

    some fixes for timing

--- src/Emulation.java
+++ src/Emulation.java
@@ -11,6 +11,7 @@ public class Emulation
 	///  TIMER  ///
 	long now = 0; // init
 	long lastTime = 0; // init
+	long million = 1_000_000; // microsec template
 	
 	/// PRINT INSTRUCTION
 	PrintTrace pTrace;
@@ -33,36 +34,38 @@ public class Emulation
 		// loop flag
 		boolean done = false;
 		
-		// Run @ 2 MHz
+		// Run @ 2 MHz (verify since untested)
 		while(!done) {	
 			// 2MHz = execute every 5e-7 secs
 			// System.nanoTime() = billionth of a sec. (epoch)
-			// nano / 1000 = millionth of a sec
-			
-			now = System.nanoTime() / 1000;
+			now = getMicroSec();
 			
 			if (lastTime == 0) {
 				lastTime = now;
 			}
 			
 			// measured in microseconds
-			
 			long elapse = now - lastTime;
 			int cycle_needed = (int) (2 * elapse);
+			
 			int cycles = 0; // reset every succeeding usec passed
 			
 			while(cycle_needed > cycles) {
 				// print instruction
-				pTrace.printInstruction(cpu.PC, true);
+				pTrace.printInstruction(cpu.PC, true); // enabling this slows (?) emulation
 			
 				// emulation
 				cycles += interpreter.emulate8080(cpu.PC);
 			}
-			lastTime = now;
 			
+			lastTime = now;
 		}
 	}
 	
+	private long getMicroSec() {
+		long microSec = System.nanoTime(); // static variable!
+		return (long) ((System.currentTimeMillis() * 1e3) + (microSec - (million * (microSec / million))) / 1000);
+	}
 	private void GenerateInterrupt() {
 		
 	}
